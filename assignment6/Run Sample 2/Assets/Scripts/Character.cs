33e2129a8c59cfa0288f9ed329fffc2b20933b34
eric0009 <zeroplam5@gmail.com>
2025-11-19 15:23:37 +0900

    6week assignment

--- assignment6/Run Sample 2/Assets/Scripts/Character.cs
+++ assignment6/Run Sample 2/Assets/Scripts/Character.cs
@@ -19,7 +19,11 @@ public class Character : MonoBehaviour
     {
         // 좌클릭시 RemainJump를 하나 소모하여 CharacterJumpPower의 힘으로 점프한다.
         // ---------- TODO ---------- 
-        
+        if (Input.GetMouseButtonDown(0) && RemainJump > 0)
+        {
+            Jump(CharacterJumpPower);
+            RemainJump--;
+        }
         // -------------------- 
     }
 
@@ -34,7 +38,8 @@ public class Character : MonoBehaviour
         // tag가 Platform인 것과 충돌하면 RemainJump를 초기화한다.
         // tag가 Obstacle인 것과 충돌하면 게임 오버한다.
         // ---------- TODO ---------- 
-        
+        if (col.gameObject.tag == "Platform") RemainJump = MaxJump;
+        else if (col.gameObject.tag == "Obstacle") GM.GameOver();
         // -------------------- 
     }
 
@@ -42,7 +47,11 @@ public class Character : MonoBehaviour
     {
         // tag가 Point인 것과 충돌하면 Point를 하나 얻고, 충돌한 오브젝트를 삭제한다.
         // ---------- TODO ---------- 
-        
+        if (col.gameObject.tag == "Point") {
+            GM.GetPoint(1);
+            Destroy(col.gameObject);
+        }
+        else if (col.gameObject.tag == "Obstacle") GM.GameOver();
         // -------------------- 
     }
 }
--- assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
+++ assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
@@ -36,7 +36,9 @@ public class GameManager : MonoBehaviour
     {
         // CamObj는 Character의 x, y position을 따라간다.
         // ---------- TODO ---------- 
-        
+        CamObj.transform.position = new Vector3(Character.transform.position.x
+        ,Character.transform.position.y,
+        Character.transform.position.z);
         // -------------------- 
     }
 
@@ -44,7 +46,7 @@ public class GameManager : MonoBehaviour
     {
         // Character는 초당 CharacterSpeed의 속도로 우측으로 움직인다.
         // ---------- TODO ---------- 
-        
+        Character.transform.Translate(Vector3.right * Time.deltaTime * CharacterSpeed);
         // -------------------- 
     }
 
@@ -52,7 +54,9 @@ public class GameManager : MonoBehaviour
     {
         // Character를 삭제하고, "Game Over!"라는 메시지를 3초간 띄우고, RestartButton을 활성화한다.
         // ---------- TODO ---------- 
-        
+        Destroy(Character);
+        MyUIManager.DisplayMessage("Game Over!", 3f);
+        MyUIManager.RestartButton.SetActive(true);
         // -------------------- 
     }
 
@@ -60,7 +64,8 @@ public class GameManager : MonoBehaviour
     {
         // point만큼 점수를 증가시키고 UI에 표시한다.
         // ---------- TODO ---------- 
-        
+        NowScore += point;
+        MyUIManager.DisplayScore(NowScore);
         // -------------------- 
     }
 
